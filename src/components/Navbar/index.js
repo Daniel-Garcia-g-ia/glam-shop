682276cee250552e69b845d06d1acd9a0f8fea02
Daniel Garcia <daniel.g.1393@gmail.com>
2023-09-12 19:51:07 -0500

    [add] navbar, icon, search

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,29 @@
 import React from "react";
+import Logo from "./logo";
+import { HiSearch } from "react-icons/hi";
+
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <>
       <nav className="navbar">
-        
-        <div>
-        
+        <div className="Logo">
+          <Logo width={130} height={130} />
+        </div>
+
+        <div className="container-item">
           <ul className="Item">
-          <h1>Glam Shop</h1>
             <li>Home</li>
             <li>Products</li>
             <li>About</li>
             <li>Pricing</li>
           </ul>
         </div>
-        <div>
+        <div className="container-search">
+          
           <input className="NavSearch" placeholder="Busca productos y mas" />
+          <span>{<HiSearch size={24}/>}</span>
         </div>
       </nav>
     </>
