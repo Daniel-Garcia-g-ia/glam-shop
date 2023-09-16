8d8d4dd15e6eb592189201c3955bf837d91a71bd
Daniel Garcia <daniel.g.1393@gmail.com>
2023-09-15 21:53:35 -0500

    [add] basic card

--- src/components/aside/index.js
+++ src/components/aside/index.js
@@ -1,27 +1,19 @@
 import React from "react";
 import Card from "../card"
-import "./Aside.css"
 
-function Aside() {
 
+function Aside()  {
     return (
-
-        <>
-
-            <div className="aside">
-                <div className="container-card">
-
-                    <Card />
-                    
-                    
-                </div>
-
-
+      <div className="container is-max-desktop">
+        <div className="columns is-centered">
+          <div className="column">
+            <div className="is-flex is-justify-content-center">
+              <Card />
             </div>
-
-        </>
-    )
-
-}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 export default Aside
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,40 +1,38 @@
 import React from "react";
-import Imagen from "../../assets/images/imagen1.webp";
+import Imagen from "../../assets/images/editada.webp";
+import { BsFillCartPlusFill } from "react-icons/bs"
 import "./Card.css"
 
 function Card() {
 
     return (
         <>
-
-            <div className="card">
-
-                <div className="container-image">
-                    <img src={Imagen} alt="Product Image" className="image" />
+            <div className="width bg box my-6 media is-hover" >
+                <div class="image-container">
+                    <img src={Imagen} alt="Product Image" />
                 </div>
-
-                <div className="container-description">
-                    <h2 className="titulo">✨El Ramo de Belleza: Un Regalo Radiante</h2>
-                    <p>
-                        Este no es un regalo ordinario, es una explosión de belleza en cada pincelada.
-                        El Ramo de Belleza es como un jardín secreto de glamour,
-                        esperando deslumbrarte. ¿Listo para vivir una experiencia de belleza excepcional?
+                <div className="px-6 py-4 ">
+                    <p className="is-size-5">
+                        ✨Gran Combo! Regalo 
                     </p>
-                    
+                    <div className="px-2 pt-6">
+                        <span className="has-text-dark is-size-3" >$10.000</span>
+                    </div>
 
-                    <span>$110.000</span>
+                    <div className="mt-5 is-container-button">
+                        <button className="button bg-button"  >Agregar al Carrito </button>
+                        <span><BsFillCartPlusFill size={30} /></span>
+                    </div>
+
+
+
+
+
+
+
+
                 </div>
-
-
-
-
-
-
-
-
-
-            </div>
-
+            </div >
         </>
     )
 
